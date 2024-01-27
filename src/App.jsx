92f1df5490e4a0e340f56c55b395c9ba984b01c9
David <davidmv1185@gmail.com>
2024-01-26 17:00:47 -0800

    border

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+import "./index.css";
+
 // Write your Color component here
+// const Color = (props) => {
+//   return <div className={props.color}></div>
+// };
+
+//or using object deconstruction
+const Color = ({color, setSelectedColor, selectedColor}) => {
+  const isSelected = color === selectedColor;
+  return (<div className={`${color} ${isSelected ? "selected" : ""}`} onClick={() => setSelectedColor(color)} ></div>
+  )
+}
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("")
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list" >
+        <Color color="blue" setSelectedColor={setSelectedColor} selectedColor={selectedColor} />
+        <Color color="violet" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+        <Color color="green" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+      </div>
     </div>
   );
 };
